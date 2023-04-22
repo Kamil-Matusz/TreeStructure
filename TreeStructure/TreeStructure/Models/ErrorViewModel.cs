a482cd6b980ad3a580529e3ba6ccc8035b212916
Awahir <matuszkamil076@gmail.com>
2023-04-22 10:13:44 +0200

    Initial commit

---
+++ TreeStructure/TreeStructure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TreeStructure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
