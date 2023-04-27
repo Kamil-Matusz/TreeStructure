dd24b8cac980ab5f3d5467003eaa6abb770e77d4
Awahir <matuszkamil076@gmail.com>
2023-04-27 18:54:08 +0200

    Refactor controller and Views

--- TreeStructure/TreeStructure/Controllers/TreeController.cs
+++ TreeStructure/TreeStructure/Controllers/TreeController.cs
@@ -13,92 +13,12 @@ namespace TreeStructure.Controllers
         {
             _dbContext = dbContext;
         }
+        
         public ActionResult Index()
         {
-            var TreeList = _dbContext.Trees.Where(x => x.ParentId == null)
-                .Select(x => new
-                {
-                    x.TreeId,
-                    x.Name
-                }).ToList();
-                ViewBag.TreeList = TreeList;
-            TreeHierarchy();
-            return View();
+            List<Tree> treeList = new();
+            treeList = _dbContext.Trees.OrderBy(x => x.TreeId).ToList();
+            return View(treeList);
         }
-
-        public JsonResult TreeHierarchy()
-        {
-            List<Tree> Tree = new();
-            List<TreeViewModel> TreeRecords = new();
-
-            Tree = _dbContext.Trees.ToList();
-
-            TreeRecords = Tree.Where(x => x.ParentId is null)
-                .Select(t => new TreeViewModel
-                {
-                    Id = t.TreeId,
-                    Text = t.Name,
-                    ParentId = t.ParentId,
-                    Childrens = GetTreeChildrens(Tree, t.TreeId)
-                }).ToList();
-
-            return Json(TreeRecords);
-        }
-
-        private List<TreeViewModel> GetTreeChildrens(List<Tree> TreeList, int parentId)
-        {
-            return TreeList.Where(x => x.ParentId == parentId)
-                .Select(t => new TreeViewModel
-                {
-                    Id = t.TreeId,
-                    Text = t.Name,
-                    ParentId = parentId,
-                    Childrens = GetTreeChildrens(TreeList,t.TreeId)
-                }).ToList();
-        }
-
-        [HttpPost]
-        public JsonResult DeleteTreeNode(string values)
-        {
-            var id = values.Split(',');
-            foreach(var item in id)
-            {
-                int TreeId = int.Parse(item);
-                _dbContext.Trees.RemoveRange(_dbContext.Trees.Where(x => x.TreeId == TreeId).ToList());
-                _dbContext.SaveChanges();
-            }
-
-            return Json(new { success = true });
-        }
-
-        [HttpPost]
-        public JsonResult ChangeNode(int nodeId,int parentId)
-        {
-            var Node = _dbContext.Trees.First(x => x.TreeId==nodeId);
-            Node.ParentId = parentId;
-            _dbContext.SaveChanges();
-
-            return Json(new { success = true });
-        }
-
-        /*[HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult AddNode(TreeNode node)
-        {
-            if(ModelState.IsValid)
-            {
-                Tree tree = new Tree()
-                {
-
-                    Name = node.NodeName,
-                    ParentId = node.ParentId,
-                };
-
-                _dbContext.Trees.Add(tree);
-                _dbContext.SaveChanges();
-            }
-
-            return Json(new { success = true });
-        }*/
     }
 }
