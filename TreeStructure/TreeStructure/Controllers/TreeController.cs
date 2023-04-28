a9d786ccc3a40e275ae0bb5b1c8da433c45b4726
Awahir <matuszkamil076@gmail.com>
2023-04-28 21:39:15 +0200

    Creating method which add new node to tree

--- TreeStructure/TreeStructure/Controllers/TreeController.cs
+++ TreeStructure/TreeStructure/Controllers/TreeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Threading.Tasks.Dataflow;
 using TreeStructure.DAL;
 using TreeStructure.Entities;
@@ -19,6 +20,28 @@ namespace TreeStructure.Controllers
             List<Tree> treeList = new();
             treeList = _dbContext.Trees.OrderBy(x => x.TreeId).ToList();
             return View(treeList);
+        }
+
+        [HttpGet]
+        public ActionResult CreateTreeNode()
+        {
+            DropDownList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateTreeNode(Tree tree) 
+        {
+            _dbContext.Trees.Add(tree);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        private void DropDownList()
+        {
+            List<SelectListItem> nodes = new SelectList(_dbContext.Trees,"TreeId","Name").ToList();
+            ViewBag.Nodes = nodes;
+
+
         }
     }
 }
